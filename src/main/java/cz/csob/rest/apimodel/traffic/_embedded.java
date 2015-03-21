4b800a572ac5f7c394f2cc1ca6beba8fe252f30a
Jan Toť <tot.jan@gmail.com>
2015-03-21 14:17:34 +0100

    add Traffic Pojos

---
+++ src/main/java/cz/csob/rest/apimodel/traffic/_embedded.java
@@ -0,0 +1,70 @@
+package cz.csob.rest.apimodel.traffic;
+
+public class _embedded
+{
+    private String system;
+
+    private String node;
+
+    private String layer;
+
+    private Action action;
+
+    private Actor actor;
+
+    public String getSystem ()
+    {
+        return system;
+    }
+
+    public void setSystem (String system)
+    {
+        this.system = system;
+    }
+
+    public String getNode ()
+    {
+        return node;
+    }
+
+    public void setNode (String node)
+    {
+        this.node = node;
+    }
+
+    public String getLayer ()
+    {
+        return layer;
+    }
+
+    public void setLayer (String layer)
+    {
+        this.layer = layer;
+    }
+
+    public Action getAction ()
+    {
+        return action;
+    }
+
+    public void setAction (Action action)
+    {
+        this.action = action;
+    }
+
+    public Actor getActor ()
+    {
+        return actor;
+    }
+
+    public void setActor (Actor actor)
+    {
+        this.actor = actor;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "ClassPojo [system = "+system+", node = "+node+", layer = "+layer+", action = "+action+", actor = "+actor+"]";
+    }
+}
