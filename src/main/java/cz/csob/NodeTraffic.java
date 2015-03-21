6b508ab963d1c403feb2cc1474e4147e7dfbc2b6
Jan Toť <tot.jan@gmail.com>
2015-03-21 16:34:03 +0100

    add Traffic Service (the rest)

---
+++ src/main/java/cz/csob/NodeTraffic.java
@@ -0,0 +1,27 @@
+package cz.csob;
+
+import cz.csob.rest.TrafficJsonService;
+
+import cz.csob.rest.apimodel.traffic.Events;
+
+
+public class NodeTraffic {
+
+    private TrafficJsonService service;
+
+    public NodeTraffic (TrafficJsonService service) {
+        this.service = service;
+    }
+
+    public String getData (String nodeId) {
+        String response = "";
+        for (int i = 0; i < service.getTrafficResponse().get_embedded().getEvents().length; i++) {
+            Events event = service.getTrafficResponse().get_embedded().getEvents()[i];
+            if (nodeId == event.get_embedded().getNode()) {
+                System.out.println(response);
+                response += event.toString();
+            }
+        }
+        return response;
+    }
+}
